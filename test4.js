dbff0648af2fbfd72d6d770b89798d16a3fb8d7c
Bruno Ferreira <bmibferreira@gmail.com>
2018-08-20 18:10:55 +0100

    Create test4.js

---
+++ test4.js
@@ -0,0 +1,21 @@
+eval('');
+
+if(1 == 2) { console.log("Cenas"); }
+
+debugger;
+
+console.log("");
+
+var z = 1.2;
+
+if (1 == 3) { console.log(""); }
+
+
+var t = 2 == 3;
+
+eval('zzzzzz');
+
+var dasdz = 2 == 3;
+
+  var alksdjas = 4;
+ var z = 2 == 2;
