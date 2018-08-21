26d0e85c994d964a9115c2877a198798a3f3ca6e
Bruno Ferreira <bmibferreira@gmail.com>
2018-08-21 09:08:58 +0100

    Create test501.js

---
+++ test501.js
@@ -0,0 +1,19 @@
+eval('ssadasdasd');
+
+if(1 == 2) { console.log("Cenas"); }
+
+debugger;
+
+while(true){
+alert("hey!!!!");
+console.error("123213123");
+console.log("1231231231233333");
+}
+
+
+var tztz = 3.2 == 32;
+var z = 3-1;
+var xpto= 0 == '0';
+
+if(1 == 3) { console.log("Test"); eval("var zzz = 123");}
+
