4b4e1b44c1538220521f68ada9c5e9f8665ddfd2
Bruno Ferreira <bmibferreira@gmail.com>
2018-08-21 07:51:36 +0100

    Create testxpto.js

---
+++ testxpto.js
@@ -0,0 +1,16 @@
+eval('dasdasd');
+
+if(1 == 2) { console.log("Cenas"); }
+
+debugger;
+
+console.log("");
+
+alert("123");
+
+while(true){
+if(1==2){
+console.err("123123");
+alert("12312333" == 3);
+}
+}
